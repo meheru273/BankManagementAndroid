b81f27282c688b2a1207b0dbebecd4b045e0457d
meheru273 <meherujannat@gmail.com>
2024-05-26 01:38:45 +0600

    final ar pari na vai

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
+    testImplementation("org.testng:testng:6.9.6")
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
